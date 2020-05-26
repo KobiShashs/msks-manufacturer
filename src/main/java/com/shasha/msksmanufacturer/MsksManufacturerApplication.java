4234c6a3ecbdd0cfe96ec283b96df9e77e760e43
Kobi <kobi.shasha@gmail.com>
2020-05-27 01:37:57 +0300

    Baby steps

---
+++ src/main/java/com/shasha/msksmanufacturer/MsksManufacturerApplication.java
@@ -0,0 +1,13 @@
+package com.shasha.msksmanufacturer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsksManufacturerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsksManufacturerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shasha/msksmanufacturer/MsksManufacturerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shasha.msksmanufacturer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsksManufacturerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
